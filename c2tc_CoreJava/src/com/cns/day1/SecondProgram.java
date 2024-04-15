85a3c2217abd5b8ff03164968981b3afb4beda9d
ELCOT <ELCOT@Lenovo>
2024-04-15 18:35:46 +0530

    sample 2

---
+++ c2tc_CoreJava/src/com/cns/day1/SecondProgram.java
@@ -0,0 +1,10 @@
+package com.cns.day1;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello wrold");
+	}
+
+}
